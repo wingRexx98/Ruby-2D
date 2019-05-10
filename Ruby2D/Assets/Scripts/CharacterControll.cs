ab1be9e64cb47a3bd5a5e62a6e82d95ba8aea9d6
wingRexx98 <47964600+wingRexx98@users.noreply.github.com>
2019-05-10 14:00:36 +0700

    character controll done

---
+++ Ruby2D/Assets/Scripts/CharacterControll.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControll : MonoBehaviour
+{
+    public float speed = 5f;//speed too smal the character won't move
+    private Rigidbody2D rigidbody2;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2 = GetComponent<Rigidbody2D>();
+        //QualitySettings.vSyncCount = 0;
+        //Application.targetFrameRate = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+
+    private void FixedUpdate()
+    {
+        float hortizontalinput = Input.GetAxis("Horizontal");
+        Debug.Log(hortizontalinput);
+        rigidbody2.velocity = new Vector2(hortizontalinput * speed * Time.fixedDeltaTime, rigidbody2.velocity.y);
+    }
+}
